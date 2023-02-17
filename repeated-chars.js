dd9908ef74b7074cfe1577dc25ab195bf2fec527
Arin Melikyan <arin.melkn@gmail.com>
2023-02-17 17:21:44 +0400

    add repeated-chars solution

---
+++ repeated-chars.js
@@ -0,0 +1,15 @@
+function commonChars(names) {
+  if (!names.length) {
+    return new Set();
+  }
+  let chars = new Set(names[0]);
+  for (let i = 1; i < names.length; i++) {
+    let nameChars = new Set(names[i]);
+    chars = new Set([...chars].filter(char => nameChars.has(char)));
+  }
+  const result = Array.from(chars);
+  return result;
+}
+
+const a = commonChars(['leslie', 'nellie', 'stella'])
+console.log(a);
